fef4899fab8687f5479e242cc449f08982b1734c
IzzyNinja <izzy@itninjas.tech>
2025-12-11 16:43:07 -0500

    Update hv2.js

--- hv2.js
+++ hv2.js
@@ -1,27 +1,30 @@
 class BotpressWebchat extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
   connectedCallback() {
-    // Prevent duplicate loads
     if (this._loaded) return;
     this._loaded = true;
 
-    // Create the internal container where Botpress will mount the chat
+    // Botpress container inside shadow root
     const container = document.createElement("div");
     container.id = "bp-embedded-webchat";
-    this.appendChild(container);
+    this.shadowRoot.appendChild(container);
 
-    // Load Botpress webchat inject script
+    // Botpress inject script
     const script1 = document.createElement("script");
     script1.src = "https://cdn.botpress.cloud/webchat/v3.5/inject.js";
     script1.async = true;
-    container.appendChild(script1);
+    this.shadowRoot.appendChild(script1);
 
-    // Load your specific Botpress config script
+    // Botpress config script
     const script2 = document.createElement("script");
     script2.src = "https://files.bpcontent.cloud/2025/12/10/22/20251210223044-P2TZOC4V.js";
     script2.defer = true;
-    container.appendChild(script2);
+    this.shadowRoot.appendChild(script2);
   }
 }
 
-// Register the custom element
 customElements.define("botpress-webchat", BotpressWebchat);
